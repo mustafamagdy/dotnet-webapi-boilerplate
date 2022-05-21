b1188e133cf82879d3f2a77d1281c6b229bb1590
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-05-21 19:59:22 +0300

    missing migration for removed ValidTo field from tenant

---
+++ src/Migrators/Migrators.MySQL/Migrations/Tenant/20220521154543_RemoveValidToFromTenant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.MySQL.Migrations.Tenant
+{
+    public partial class RemoveValidToFromTenant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValidUpto",
+                schema: "MultiTenancy",
+                table: "Tenants");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidUpto",
+                schema: "MultiTenancy",
+                table: "Tenants",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
